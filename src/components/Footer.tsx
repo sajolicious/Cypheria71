644f4d5848f762c93adc379074bd2516927545e4
Sajol Das <sajoldas421@example.com>
2023-12-25 04:14:00 +0600

    react-leaflet

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
-import { FaPhone, FaEnvelope, FaMapMarkerAlt, FaFacebook, FaTwitter, FaLinkedin, FaInstagram } from 'react-icons/fa';
+import React from "react";
+import {
+  FaPhone,
+  FaEnvelope,
+  FaMapMarkerAlt,
+  FaFacebook,
+  FaTwitter,
+  FaLinkedin,
+  FaInstagram,
+} from "react-icons/fa";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import 'leaflet/dist/leaflet.css';
 
 interface FooterProps {
-  // Add any required props here
+  
 }
 
 const Footer: React.FC<FooterProps> = () => {
+  const position = [23.174194, 90.1739155]; 
+  
   return (
-    <div className="flex flex-col space-y-5 bg-black pt-[200px]">
+    <div className="flex flex-col space-y-5 bg-black pt-[10px]">
       <div className="md:flex md:flex-row lg:w-[90%] lg:mx-auto flex-col space-y-10 px-4 lg:px-8 md:justify-between">
         <div className="flex flex-col space-y-7 md:space-y-4">
           <div className="font-libre text-[40px] text-start font-bold text-white">
@@ -24,7 +36,9 @@ const Footer: React.FC<FooterProps> = () => {
             </div>
             <div className="font-libre text-[16px] font-normal  text-white">
               <FaMapMarkerAlt className="w-6 h-6 mr-2 inline-block" />
-              Dhanmondi32<br /> Dhaka<br /> 
+              Dhanmondi32
+              <br /> Dhaka
+              <br />
               Khulna, Bangladesh
             </div>
           </div>
@@ -36,19 +50,32 @@ const Footer: React.FC<FooterProps> = () => {
               placeholder="Enter Your email"
               className="outline-none border-b-2 border-gray-500 w-[300px] mx-auto bg-transparent"
             />
-            <img className="absolute right-0 top-3" src="img/paperPlane.png" alt="" />
+            <img
+              className="absolute right-0 top-3"
+              src="img/paperPlane.png"
+              alt=""
+            />
           </div>
           <div>
-            <p className="text-lg text-white pb-2">Subscribe to our Newsletter</p>
+            <p className="text-lg text-white pb-2">
+              Subscribe to our Newsletter
+            </p>
           </div>
           <div className="w-fit mx-auto">
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d7335.781124239648!2d90.1739155!3d23.174194000000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1697010536322!5m2!1sen!2sbd"
-              allowFullScreen
-              loading="lazy"
-              referrerPolicy="no-referrer-when-downgrade"
-              className="w-[300px] rounded-lg"
-            />
+            {/* Use react-leaflet for the map */}
+            <MapContainer
+              center={position}
+              zoom={13}
+              style={{ width: "300px", height: "300px" }}
+            >
+              <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              />
+              <Marker position={position}>
+                <Popup>Your location</Popup>
+              </Marker>
+            </MapContainer>
           </div>
           <div className="flex justify-between gap-2">
             <p className="lg:text-[24px] text-[16px] font-popp font-medium text-white">
